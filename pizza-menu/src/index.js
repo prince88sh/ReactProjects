19a9032b5c7bfcf05eb52622f3a87947f11cf0e5
PRINCE SHARMA <prince159sh@gmail.com>
2024-07-23 10:02:19 +0530

    Added component

---
+++ pizza-menu/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
+}
+function Pizza() {
+  return <h2>Pizza</h2>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
