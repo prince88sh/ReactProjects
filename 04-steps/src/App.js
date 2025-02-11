d8c2f5e1bbca87d2b54276677cc8d10893c96bcf
PRINCE SHARMA <prince88sh>
2025-02-11 09:26:57 +0530

    using useState for preactice in new react app

---
+++ 04-steps/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  // const step = 1;
+
+  const [step, setStep] = useState(1);
+  /*REMEBER usestate is an arra [variable, and its fucntions]
+  [
+    0:1,
+    1:f()
+  ]
+    */
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}> 2</div>
+        <div className={step >= 3 ? "active" : ""}> 3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          // onClick={() => {
+          //   alert("clciked on previous");
+          // }} this is the one way to use handle Event function
+          onClick={handlePrevious}
+        >
+          PREVIOUS
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          NEXT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
